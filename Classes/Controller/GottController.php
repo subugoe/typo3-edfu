6437712814963b11ca90a27bf48c75cabd969dbb
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-08-08 12:59:18 +0200

    Added stelle functionality to god

--- Classes/Controller/GottController.php
+++ Classes/Controller/GottController.php
@@ -32,7 +32,7 @@ namespace Ipf\Edfu\Controller;
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class GottController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+class GottController extends EdfuController {
 
 	/**
 	 * godRepository
@@ -66,6 +66,7 @@ class GottController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 		$this->view
 				->assign('gods', $gods)
 				->assign('baende', $baende)
+				->assign('jQuery', $this->jQueryPath)
 				->assign('position', $this->position);
 	}
 
